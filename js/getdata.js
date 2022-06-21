469e6a0cfc2be36d6577de2de013b1bfa2eb396f
Milad_Mrk <milad.rastgoou@gmail.com>
2022-06-21 22:24:26 +0430

    [feat] ajax

--- js/getdata.js
+++ js/getdata.js
@@ -2,29 +2,27 @@ $(document).ready(function () {
   $("#get-data-form").submit(function (e) {
     var content = tinymce.get("texteditor").getContent();
 
-    $("#data-container").html(content);
+    // $("#data-container").html(content);
 
-    const html = $("#data-container").html();
-    console.log(html);
-
-    const jsonHTML = JSON.stringify({ content: html });
+    const jsonHTML = JSON.stringify({ content: content });
     console.log(jsonHTML);
+    console.log(content);
 
-    // $.ajax({
-    //   url: "AJAX_POST_URL",
-    //   type: "POST",
-    //   data: jsonHTML,
-    //   success: function (data, textStatus, jqXHR) {
-    //     //data - response from server
-    //   },
-    //   error: function (jqXHR, textStatus, errorThrown) {},
-    // });
+    $.ajax({
+      url: "http://localhost:3005/content",
+      type: "POST",
+      data: { html: content },
+      success: function (data, textStatus, jqXHR) {
+        //data - response from server
+        console.log("Ok");
+      },
+      error: function (jqXHR, textStatus, errorThrown) {},
+    });
 
     //showHtml
-
     const parsHTML = JSON.parse(jsonHTML);
-    console.log(parsHTML);
-    $("#data-container").parsHTML;
+
+    $("#data-container").html(parsHTML.content);
     return false;
   });
 });
